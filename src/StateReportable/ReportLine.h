1929e79c6d492fddaa830492afca474b0792b421
Alexander Dubovik <dubovik_a_d@mail.ru>
2019-06-07 16:40:26 +0300

    ReportLine refactoring and test

--- src/StateReportable/ReportLine.h
+++ src/StateReportable/ReportLine.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <ostream>
-#include <sstream>
 #include <string>
-#include <optional>
 
 
 namespace StateReportable
@@ -14,8 +12,33 @@ namespace StateReportable
     std::string typeName;
     std::string stateFrom;
     std::string stateTo;
-    // TODO: why optional?
-    std::optional<long long> durationMicroSeconds;
+    long long duration;
+
+    ReportLine() = delete;
+
+    // Perfect forwarding for strings.
+    // User should provide all the data in c-tor to avoid non-initialized fields.
+    template<typename T1, typename T2, typename T3>
+    ReportLine(T1 typeName_, T2 stateFrom_, T3 stateTo_, long long duration_)
+        : typeName(std::forward<T1>(typeName_)),
+        stateFrom(std::forward<T2>(stateFrom_)),
+        stateTo(std::forward<T3>(stateTo_)),
+        duration(duration_)
+    {}
+    ~ReportLine() = default;
+
+    ReportLine(const ReportLine &) = default;
+    ReportLine(ReportLine &&) = default;
+
+    ReportLine& operator=(const ReportLine &) = default;
+    ReportLine& operator=(ReportLine &&) = default;
+
+  private:
+    auto rank() const { return std::tie(typeName, stateFrom, stateTo, duration); };
+
+  public:
+    bool operator==(const ReportLine &oth) const { return rank() == oth.rank(); };
+    bool operator!=(const ReportLine &oth) const { return rank() != oth.rank(); };
   };
 
   std::ostream &operator<<(std::ostream &stream, const StateReportable::ReportLine &reportLine);
--- src/tests/ReportLineTest/ReportLineTest.cpp
+++ src/tests/ReportLineTest/ReportLineTest.cpp
@@ -1,9 +1,49 @@
 #include <gtest/gtest.h>
 
+#include "ReportLine.h"
+
 
 TEST(ReportLineTest, Test_01)
 {
-  EXPECT_TRUE(true);
+  auto to = std::string("to");
+  // mixed char*, rvalue and lvalue
+  auto line = StateReportable::ReportLine("name", std::string("from"), to, 2759872395379);
+  EXPECT_EQ(line.typeName, "name");
+  EXPECT_EQ(line.stateFrom, "from");
+  EXPECT_EQ(line.stateTo, "to");
+  EXPECT_EQ(line.duration, 2759872395379);
+}
+
+
+TEST(ReportLineTest, Test_02)
+{
+  auto line = StateReportable::ReportLine("name", "from", "to", 652938768739528);
+  std::ostringstream buf;
+  buf << line;
+  EXPECT_EQ(buf.str(), "name\tfrom\tto\t652938768739528");
+}
+
+
+TEST(ReportLineTest, Test_03)
+{
+  auto expected = StateReportable::ReportLine("name", "from", "to", 652938768739528);
+  std::ostringstream buf;
+  buf << expected;
+  EXPECT_NO_THROW(
+    auto result = StateReportable::convertToReportLine(buf.str());
+    EXPECT_EQ(result, expected);
+  );
+}
+
+
+TEST(ReportLineTest, Test_04)
+{
+  auto expected = StateReportable::ReportLine("name", "from", "to", 652938768739528);
+
+  EXPECT_NE(expected, StateReportable::ReportLine("jhgj", "from", "to", 652938768739528));
+  EXPECT_NE(expected, StateReportable::ReportLine("name", "yfuewt", "to", 652938768739528));
+  EXPECT_NE(expected, StateReportable::ReportLine("name", "from", "iuyiye", 652938768739528));
+  EXPECT_NE(expected, StateReportable::ReportLine("name", "from", "to", 8765893728273767));
 }
 
 
