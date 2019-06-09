adb7108688790a9d2832660df842249c1cf9772f
Alexander Dubovik <dubovik_a_d@mail.ru>
2019-06-09 21:58:22 +0300

    statMaker advance

--- src/StateReportable/toFile/statMaker.cpp
+++ src/StateReportable/toFile/statMaker.cpp
@@ -6,10 +6,40 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <atomic>
+
+
+// Ctrl-C flag
+static std::atomic<bool> interrupted = false;
+
+
+#ifdef _WIN32
+#include <windows.h>
+BOOL WINAPI consoleHandler(DWORD)
+{
+  interrupted = true;
+  return TRUE;
+}
+#else
+#include <csignal>
+void consoleHandler(int s)
+{
+  interrupted = true;
+}
+#endif
 
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+  // For Ctrl-C processing
+  SetConsoleCtrlHandler(consoleHandler, TRUE);
+#else
+  struct sigaction sigIntHandler { {consoleHandler}};
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigaction(SIGINT, &sigIntHandler, nullptr);
+#endif
+
   if ( argc != 2 )
   {
     std::cout << "Please specify raw statistics file name as the first command line argument." << std::endl;
@@ -18,7 +48,7 @@ int main(int argc, char *argv[])
 
   std::ifstream file(argv[1]);
   std::vector<StateReportable::core::ReportLine> rawStat;
-  while ( !file.eof() )
+  while ( !file.eof() && !interrupted )
   {
     std::string str;
     std::getline(file, str);
@@ -30,9 +60,12 @@ int main(int argc, char *argv[])
     }
   }
 
-  auto stat = StateReportable::core::calcStat(rawStat);
+  if ( !interrupted )
+  {
+    auto stat = StateReportable::core::calcStat(rawStat);
 
-  std::cout << stat;
+    std::cout << stat;
+  }
 
   return 0;
 }
